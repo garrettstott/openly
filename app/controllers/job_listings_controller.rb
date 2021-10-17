1b5b7887932da8ad888ffce19835fab01dcf316f
garrettstott <garrettstott@gmail.com>
2021-10-17 13:30:44 -0600

    jobs index, styles

--- app/controllers/job_listings_controller.rb
+++ app/controllers/job_listings_controller.rb
@@ -1,11 +1,19 @@
 class JobListingsController < ApplicationController
 
+  before_action :set_company, only: [:company]
+
   def index
+    @jobs = JobListing.all
   end
 
   def company
-    @company = Company.find_by(name: params[:company_name])
     @jobs = @company.job_listings
+  end
+
+  private
+
+  def set_company
+    @company = Company.find_by(name: params[:company_name])
   end
 
 end
