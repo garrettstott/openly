c8d17a9e25eaf31d956827690a65bc559ae20e3f
garrettstott <garrettstott@gmail.com>
2022-10-30 14:21:35 -0600

    upgraded to 7.0.4

--- Gemfile
+++ Gemfile
@@ -4,11 +4,11 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '3.0.1'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
-gem 'rails', '7.0.0'
+gem 'rails', '7.0.4'
 # Use mysql2 as the database for Active Record
 gem 'mysql2', '~> 0.5'
 # Use Puma as the app server
-gem 'puma', '~> 5.0'
+gem 'puma', '~> 6.0'
 # Use SCSS for stylesheets
 gem 'sass-rails', '>= 6'
 # Transpile app-like JavaScript. Read more: https://github.com/rails/webpacker
@@ -17,7 +17,7 @@ gem 'webpacker', '~> 5.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
-gem 'redis', '~> 4.0'
+gem 'redis', '~> 5.0'
 # Use Active Model has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 gem "sprockets-rails"
@@ -32,7 +32,7 @@ gem 'turbo-rails'
 gem 'stimulus-rails'
 
 # Reduces boot times through caching; required in config/boot.rb
-gem 'bootsnap', '>= 1.4.4', require: false
+gem 'bootsnap', '>= 1.13.0', require: false
 
 group :development, :test do
   gem 'pry'
--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_07_180625) do
-
+ActiveRecord::Schema[7.0].define(version: 2022_10_07_180625) do
   create_table "addresses", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "addressable_id", null: false
     t.string "addressable_type", null: false
@@ -21,8 +20,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.string "region", null: false
     t.string "country", null: false
     t.string "postal_code", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["addressable_id"], name: "index_addresses_on_addressable_id"
     t.index ["addressable_type"], name: "index_addresses_on_addressable_type"
   end
@@ -36,8 +35,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.boolean "current", default: true, null: false
     t.boolean "approved", default: false, null: false
     t.boolean "denied", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_chiefs_on_company_id"
     t.index ["created_by_id"], name: "index_chiefs_on_created_by_id"
   end
@@ -52,8 +51,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.integer "created_by_id", null: false
     t.boolean "approved", default: false, null: false
     t.boolean "denied", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["created_by_id"], name: "index_companies_on_created_by_id"
   end
 
@@ -65,8 +64,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.date "end_date"
     t.string "job_title", null: false
     t.integer "salary"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_employment_companies_on_company_id"
     t.index ["user_id"], name: "index_employment_companies_on_user_id"
   end
@@ -77,8 +76,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.string "salary"
     t.text "description"
     t.integer "job_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["company_id"], name: "index_job_listings_on_company_id"
   end
 
@@ -88,8 +87,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.integer "created_by_id", null: false
     t.integer "noteable_id"
     t.string "noteable_type"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["created_by_id"], name: "index_notes_on_created_by_id"
     t.index ["noteable_id"], name: "index_notes_on_noteable_id"
     t.index ["noteable_type"], name: "index_notes_on_noteable_type"
@@ -99,8 +98,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.bigint "review_id", null: false
     t.integer "style", null: false
     t.integer "score", default: 0, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["review_id"], name: "index_ratings_on_review_id"
   end
 
@@ -113,8 +112,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.float "overall_rating", default: 0.0, null: false
     t.boolean "approved", default: false, null: false
     t.boolean "denied", default: false, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["chief_id"], name: "index_reviews_on_chief_id"
     t.index ["company_id"], name: "index_reviews_on_company_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
@@ -124,20 +123,20 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
     t.integer "sign_in_count", default: 0, null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
+    t.datetime "current_sign_in_at", precision: nil
+    t.datetime "last_sign_in_at", precision: nil
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
     t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
+    t.datetime "confirmed_at", precision: nil
+    t.datetime "confirmation_sent_at", precision: nil
     t.string "unconfirmed_email"
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
-    t.datetime "locked_at"
+    t.datetime "locked_at", precision: nil
     t.string "first_name", null: false
     t.string "last_name", null: false
     t.string "gender", null: false
@@ -149,8 +148,8 @@ ActiveRecord::Schema.define(version: 2022_10_07_180625) do
     t.string "username", null: false
     t.boolean "admin", default: false, null: false
     t.integer "role"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
